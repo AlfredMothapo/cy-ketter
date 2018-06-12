3e9edfa350a34be476f3c25f4d80ef84c47482de
Alfred <alfred.mothapo@media24.com>
2018-06-12 17:13:11 +0200

    converted spec files to js

--- cypress/integration/user/login.spec.js
+++ cypress/integration/user/login.spec.js
@@ -6,7 +6,7 @@ describe("Login test",()=>{
     })
 
     it("checks if the login form is loaded",()=>{
-
+        
     })
     it("populate inputs in the login form",()=>{
 
@@ -19,5 +19,11 @@ describe("Login test",()=>{
     })
     it("Check if the user is redirected after successfull login",()=>{
 
+    })
+
+    after(()=>{
+        cy.window().then((win) => {
+            cy.spy(win.console, "log")
+          })
     })
 })
