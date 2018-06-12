9d2d3eb2f900689bffcf9c7f570f54b19b27179c
Alfred <alfred.mothapo@media24.com>
2018-06-12 16:01:04 +0200

    initial commit

---
+++ cypress/integration/user/login.spec.ts
@@ -0,0 +1,23 @@
+
+describe("Login test",()=>{
+
+    beforeEach(()=>{
+        //do something before each test
+    })
+
+    it("checks if the login form is loaded",()=>{
+
+    })
+    it("populate inputs in the login form",()=>{
+
+    })
+    it("clicks the submit button",()=>{
+
+    })
+    it("checks if validation rules passed",()=>{
+
+    })
+    it("Check if the user is redirected after successfull login",()=>{
+
+    })
+})
---
+++ cypress/integration/user/logout.spec.ts
@@ -0,0 +1,16 @@
+describe("logout test",()=>{
+
+    beforeEach(()=>{
+        //do something
+    });
+
+    it("checks if the logout button exists",()=>{
+
+    })
+    it("clicks the logout button",()=>{
+
+    })
+    it("checks if the user is redirected home",()=>{
+
+    })
+})
